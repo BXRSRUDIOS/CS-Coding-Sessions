95be481f534265fdea416cb48062b35af7fa6779
BXRSRUDIOS <bilalrajastrikes23@gmail.com>
2024-10-29 22:37:13 +0000

    Finished the Main SQL Python Tasks for the Holiday Homework

--- CS Lessons - Half Term 1/Holiday Homework/mainSQLWork.py
+++ CS Lessons - Half Term 1/Holiday Homework/mainSQLWork.py
@@ -2,14 +2,20 @@ import sqlite3 as lite
 import time as t
 db = "PupilDatav2.db"
 
+"""
+THIS CODE IS 100% NOT GONNA WORK, THIS IS ONLY OPTIMISED TO DO THE HOMEWORK AND NOT ACTUAL CONCURRENT PROCESSING WITH TRANSACTIONS HENCE WHY IF YOU RAN THIS YOU'LL SEE A TON OF DATABASE IS LOCKED TYPE ERRORS
+Lord Forgive My Messy Code And For Not Keeping My Database in Third Normal Form Because This Was How It Was Given To Me For The Homework :(
+"""
+
 def getAllRecords():
-    con = lite.connect(db)
+    con = lite.connect(db, timeout=1)
     cur = con.cursor()
     allRecords = """SELECT *
 FROM Pupils
 """
     cur.execute(allRecords)
     results = cur.fetchall()
+    cur.close()
     return results
 
 def showList(aList):
@@ -18,27 +24,30 @@ def showList(aList):
         print("\t".join([str(x) for x in row]))
 
 def twoQuerySearch(*args):
-    con = lite.connect(db)
+    con = lite.connect(db, timeout=1)
     cur = con.cursor()
     allRecords = f"{args[0]}\n{args[1]}\n"
     cur.execute(allRecords)
     results = cur.fetchall()
+    cur.close()
     return results
 
 def threeQuerySearch(*args):
-    con = lite.connect(db)
+    con = lite.connect(db, timeout=1)
     cur = con.cursor()
     allRecords = f"{args[0]}\n{args[1]}\n{args[2]}"
     cur.execute(allRecords)
     results = cur.fetchall()
+    cur.close()
     return results
 
 def fourQuerySearch(*args):
-    con = lite.connect(db)
+    con = lite.connect(db, timeout=1)
     cur = con.cursor()
     allRecords = f"{args[0]}\n{args[1]}\n{args[2]}\n{args[3]}\n"
     cur.execute(allRecords)
     results = cur.fetchall()
+    cur.close()
     return results
 
 def main():
@@ -67,10 +76,88 @@ def main():
     showList(boysDOB)
 
     # Task 6 - Adding Attribute Column for Achievement Points - Behaviour Points
-    """createDiffAttribute = twoQuerySearch("ALTER TABLE Pupils", "ADD PointsDifference")
+    createDiffAttribute = twoQuerySearch("ALTER TABLE Pupils", "ADD PointsDifference")
     boysSetupDifference = twoQuerySearch("UPDATE Pupils", "SET PointsDifference=(AchievementPoints-BehaviourPoints)")
     boysShowDifferences = twoQuerySearch("SELECT FullName, PointsDifference", "FROM Pupils")
-    showList(boysShowDifferences)"""
+    showList(boysShowDifferences)
+
+    # Task 7 - Write a query where the user enters the criteria for all pupils in a tutor group
+    selectStatement = input("Enter Select Statement")
+    fromStatement = input("Enter From Statement")
+    whereStatement = input("Enter Where Statement")
+    customStatement = threeQuerySearch(selectStatement, fromStatement, whereStatement)
+    showList(customStatement)
+
+    # Task 8 - Write a query for user to enter a house and registration group
+    house = input("Enter House")
+    regGroup = input("Enter Registration Group")
+    whereQuery = f"WHERE Hose='{house}' AND RegGroup='{regGroup}'"
+    houseRegGroup = threeQuerySearch("SELECT *", "FROM Pupils", whereQuery)
+    showList(houseRegGroup)
+    
+    # Task 9 - Write Query for user to enter Julian Day for Date of Birth
+    dob = input("Enter date of birth in YYYY-MM-DD")
+    whereQuery2 = f"WHERE DOB>'{dob}'"
+    dobQuery = threeQuerySearch("SELECT *", "FROM Pupils", whereQuery2)
+    showList(dobQuery)
+
+    # Task 10 - Write Query for user to enter Registration Group and Behaviour Points
+    behPoints = int(input("Enter Behaviour Points"))
+    regGroup = input("Enter Registration Group")
+    whereQuery3 = f"WHERE BehaviourPoints>{behPoints} AND RegGroup='{regGroup}'"
+    behRegGroup = threeQuerySearch("SELECT *", "FROM Pupils", whereQuery3)
+    showList(behRegGroup)
+
+    # Task 11 - Write a query to work out each boys lowest, highest and average test scores
+    createLowestScoreAttribute = twoQuerySearch("ALTER TABLE Pupils", "ADD LowestScore")
+    createHighestScoreAttribute = twoQuerySearch("ALTER TABLE Pupils", "ADD HighestScore")
+    createAverageScoreAttributes = twoQuerySearch("ALTER TABLE Pupils", "ADD AverageScore")
+    caseLowestStatement = """SET LowestScore = CASE
+        WHEN TestScore1 <= TestScore2 AND TestScore1 <= TestScore3 THEN TestScore1
+        WHEN TestScore2 <= TestScore1 AND TestScore2 <= TestScore3 THEN TestScore2
+        ELSE TestScore3"""
+    updateLowestStatement = threeQuerySearch("UPDATE Pupils", caseLowestStatement, "END;")
+    caseHighestStatement = """SET Highest = CASE
+        WHEN TestScore1 >= TestScore2 AND TestScore1 >= TestScore3 THEN TestScore1
+        WHEN TestScore2 >= TestScore1 AND TestScore2 >= TestScore3 THEN TestScore2
+        ELSE TestScore3"""
+    updateHighestStatement = threeQuerySearch("UPDATE Pupils", caseHighestStatement, "END;")
+    updateAverageStatement = twoQuerySearch("UPDATE Pupils", "SET AverageScore=((TestScore1+TestScore2+TestScore3)/3))")
+    highLowAverageStatement = threeQuerySearch("SELECT FullName, RegGroup, TestScore1, TestScore2, TestScore3, HighestScore, LowestScore, AverageScore")
+    showList(threeQuerySearch)
+    
+    # Task 12 - Write a query using update to make the boys scored more than 80 behaviour points less than 20 achievement points members of the Dawg house
+    dawgUpdateQuery = threeQuerySearch("UPDATE Pupils", "SET Hose='Dawg'", "WHERE BehaviourPoints > 80 AND AchievementPoints < 20")
+    dawgSearchQuery = threeQuerySearch("SELECT *", "FROM Pupils", "WHERE Hose='Dawg'")
+    showList(dawgSearchQuery)
+
+    # Task 13 - Write a query to calculate the age of each pupil
+    ageQuery = twoQuerySearch("SELECT FullName, DOB, CAST((julianday('now') - julianday(DOB)) / 365.25 AS INT) AS Age", "FROM Pupils")
+    showList(ageQuery)
+
+    # Task 14 - Write a query to extract first and last names from the full name column
+    nameQuery = twoQuerySearch("SELECT FullName, TRIM(substr(FullName, instr(FullName, ',') + 1)) AS FirstName, TRIM(substr(FullName, 1, instr(FullName, ',') - 1)) AS LastName", "FROM Pupils")
+    showList(nameQuery)
+
+    # Task 15 - Write a query to make a username based on last 2 digits of yob and first 3 chars of first and last name
+    usernameFormation = """LOWER(
+        SUBSTR(CAST(STRFTIME('%Y', DOB) AS TEXT), -2) || 
+        SUBSTR(TRIM(SUBSTR(FullName, INSTR(FullName, ',') + 1)), 1, 3) || 
+        SUBSTR(TRIM(SUBSTR(FullName, 1, INSTR(FullName, ',') - 1)), 1, 3)
+    ) AS Username"""
+    usernameQuery = twoQuerySearch(f"SELECT FullName, DOB, {usernameFormation}", "FROM Pupils")
+    showList(usernameQuery)
+
+    # Task 16 - Write a query to make a username based on last 2 digits of yob + full surname + first 2 chars of first name
+    usernameFormation2 = """LOWER(
+        SUBSTR(CAST(STRFTIME('%Y', DOB) AS TEXT), -2) || 
+        TRIM(SUBSTR(FullName, 1, instr(FullName, ',') - 1)) || 
+        SUBSTR(TRIM(SUBSTR(FullName, 1, INSTR(FullName, ',') + 1)), 1, 2)
+    ) AS Username"""
+    usernameQuery2 = twoQuerySearch(f"SELECT FullName, DOB, {usernameFormation2}", "FROM Pupils")
+    showList(usernameQuery2)
+    
+
     
 if __name__ == "__main__":
     main()
